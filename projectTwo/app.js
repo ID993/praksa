5e9d059269c492363cd6e36407f3bf5c87fbea4f
Ivo <id47498@oss.unist.hr>
2023-05-18 10:31:28 +0200

    minor refactoring

--- projectTwo/app.js
+++ projectTwo/app.js
@@ -1,6 +1,79 @@
-
 const ExcelJS = require('exceljs');
 const fs = require('fs');
+
+
+function applyRichTextFormatting(worksheet, cellRef, boldText) {
+  const cell = worksheet.getCell(cellRef);
+  const parts = [
+    { text: boldText, font: { bold: true, size: 14, horizontal: 'center' } },
+    { text: cell.value.toString().substring(boldText.length) }
+  ];
+  cell.value = { richText: parts };
+}
+
+function applyRowBorderToCells(worksheet, rowIndex, startColumn, endColumn, borderStyle) {
+  for (let col = startColumn; col <= endColumn; col++) {
+    const cell = worksheet.getCell(`${String.fromCharCode(64 + col)}${rowIndex}`);
+    cell.border = {
+      top: { style: borderStyle },
+      bottom: { style: borderStyle },
+      right: { style: borderStyle },
+      left: { style: borderStyle } };
+    cell.font = {
+      name: 'Calibri',
+      bold: true,
+      size: 11 };
+    cell.alignment = {
+      vertical: 'middle',
+      horizontal: 'center',
+      wrapText: true };
+  }
+}
+
+function applyOneRowBorderToCells(worksheet, rowIndex, startColumn, endColumn, borderStyle) {
+  for (let col = startColumn; col <= endColumn; col++) {
+    const cell = worksheet.getCell(`${String.fromCharCode(64 + col)}${rowIndex}`);
+    if (col == endColumn) {
+      cell.border = {
+          left: { style: borderStyle },
+          bottom: { style: 'medium' },
+          right: { style: 'medium' } };
+    } else {
+    cell.border = {
+      right: { style: borderStyle },
+      left: { style: borderStyle },
+      bottom: { style: 'medium' } };
+    }
+  }
+}
+
+function applyBorderToCells(worksheet, startRow, endRow, startColumn, endColumn, borderStyle) {
+  for (let row = startRow; row <= endRow; row++) {
+    for (let col = startColumn; col <= endColumn; col++) {
+      const cell = worksheet.getCell(`${String.fromCharCode(64 + col)}${row}`);
+      if (col == endColumn) {
+        cell.border = {
+            top: { style: borderStyle },
+            bottom: { style: borderStyle },
+            right: { style: 'medium' },
+            left: { style: borderStyle } };
+      } else {
+        cell.border = {
+          top: { style: borderStyle },
+          bottom: { style: borderStyle },
+          right: { style: borderStyle },
+          left: { style: borderStyle } };
+      }
+    }
+  }
+}
+
+function applyRowColorToCells(worksheet, rowIndex, startColumn, endColumn) {
+  for (let col = startColumn; col <= endColumn; col++) {
+    const cell = worksheet.getCell(`${String.fromCharCode(64 + col)}${rowIndex}`);
+    cell.fill = { type: 'pattern', pattern: 'solid', fgColor: { argb: 'DDDDDD' } };
+  }
+}
 
 async function generateExcelTable() {
   const workbook = new ExcelJS.Workbook();
@@ -21,7 +94,6 @@ async function generateExcelTable() {
     worksheet.getRow(15).height = 15;
     worksheet.getRow(16).height = 70.5;
   
-
     worksheet.getColumn('A').width = 5.89;
     worksheet.getColumn('B').width = 18.33;
     worksheet.getColumn('C').width = 21.11;
@@ -36,7 +108,6 @@ async function generateExcelTable() {
     worksheet.getColumn('L').width = 8.11;
     worksheet.getColumn('M').width = 8.11;
     worksheet.getColumn('N').width = 8.11;
-
 
     worksheet.mergeCells('A6:I11');
     worksheet.mergeCells('A12:B12');
@@ -55,7 +126,6 @@ async function generateExcelTable() {
     worksheet.mergeCells('K15:M15');
     worksheet.mergeCells('A25:C25');
 
-
     worksheet.getCell('A5').value = 'Predmet: ';
     worksheet.getCell('A6').alignment = { vertical: 'middle', wrapText: true };
     worksheet.getCell('A6').value = 'NALOG ZA ISPLATU\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
@@ -66,148 +136,55 @@ async function generateExcelTable() {
     worksheet.getCell('A34').alignment = { vertical: 'middle', wrapText: true };
     worksheet.getCell('J34').alignment = { vertical: 'middle', wrapText: true };
 
-function applyRichTextFormatting(worksheet, cellRef, boldText) {
-    const cell = worksheet.getCell(cellRef);
-    
-    const parts = [
-      { text: boldText, font: { bold: true, size: 14, horizontal: 'center' } },
-      { text: cell.value.toString().substring(boldText.length) }
-    ];
+    applyRichTextFormatting(worksheet, 'A6', 'NALOG ZA ISPLATU');
   
-    cell.value = { richText: parts };
-  }
-  
-  applyRichTextFormatting(worksheet, 'A6', 'NALOG ZA ISPLATU');
-  
+    applyRowBorderToCells(worksheet, 12, 1, 9, 'medium');
+    applyRowBorderToCells(worksheet, 13, 1, 9, 'medium');
+    applyOneRowBorderToCells(worksheet, 13, 1, 9, 'thin');
+    applyRowBorderToCells(worksheet, 15, 1, 14, 'medium');
+    applyRowBorderToCells(worksheet, 16, 1, 14, 'medium');
+    applyBorderToCells(worksheet, 17, 24, 1, 14, 'thin');
+    applyRowColorToCells(worksheet, 12, 1, 9);
+    applyRowColorToCells(worksheet, 15, 1, 14);
+    applyRowColorToCells(worksheet, 16, 1, 14);
+    applyRowBorderToCells(worksheet, 25, 1, 14, 'medium');
 
-//   const jsonData = fs.readFileSync('data.json', 'utf8');
-//   const data = JSON.parse(jsonData);
+    worksheet.getCell('A12').value = 'Katedra';
+    worksheet.getCell('C12').value = 'Studij';
+    worksheet.getCell('D12').value = 'ak. god.';
+    worksheet.getCell('E12').value = 'stud. god.';
+    worksheet.getCell('F12').value = 'početak turnusa';
+    worksheet.getCell('G12').value = 'kraj turnusa';
+    worksheet.getCell('H12').value = 'br sati predviđen programom';
+    worksheet.getCell('A15').value = 'Redni broj';
+    worksheet.getCell('B15').value = 'Nastavnik/Suradnik';
+    worksheet.getCell('C15').value = 'Zvanje';
+    worksheet.getCell('D15').value = 'Status';
+    worksheet.getCell('E15').value = 'Sati nastave';
+    worksheet.getCell('E16').value = 'pred';
+    worksheet.getCell('F16').value = 'sem';
+    worksheet.getCell('G16').value = 'vjež';
+    worksheet.getCell('H15').value = 'Bruto satnica predavanja (EUR)';
+    worksheet.getCell('I15').value = 'Bruto satnica seminari (EUR)';
+    worksheet.getCell('J15').value = 'Bruto satnica vježbe (EUR)';
+    worksheet.getCell('K15').value = 'Bruto iznos';
+    worksheet.getCell('K16').value = 'pred';
+    worksheet.getCell('L16').value = 'sem';
+    worksheet.getCell('M16').value = 'vjež';
+    worksheet.getCell('N15').value = 'Ukupno za isplatu (EUR)';
+    worksheet.getCell('A25').value = 'UKUPNO';
 
-
-//   data.forEach((row, index) => {
-//     const rowIndex = index + 12; // Add 12 to account for the preceding rows
-//     worksheet.addRow(row).commit();
-//   });
-
-
-  const greyFill = { type: 'pattern', pattern: 'solid', fgColor: { argb: 'DDDDDD' } };
-  const boldFont = { bold: true };
-  const boldBorder = { style: 'medium' };
-
-  function applyRowBorderToCells(worksheet, rowIndex, startColumn, endColumn, borderStyle) {
-    for (let col = startColumn; col <= endColumn; col++) {
-      const cell = worksheet.getCell(`${String.fromCharCode(64 + col)}${rowIndex}`);
-      cell.border = {
-        top: { style: borderStyle },
-        bottom: { style: borderStyle },
-        right: { style: borderStyle },
-        left: { style: borderStyle }
-      };
-      cell.font = {
-        name: 'Calibri',
-        bold: true,
-        size: 11
-      };
-      cell.alignment = {
-        vertical: 'middle',
-        horizontal: 'center',
-        wrapText: true
-        };
-    }
-  }
-
-  function applyOneRowBorderToCells(worksheet, rowIndex, startColumn, endColumn, borderStyle) {
-    for (let col = startColumn; col <= endColumn; col++) {
-      const cell = worksheet.getCell(`${String.fromCharCode(64 + col)}${rowIndex}`);
-      if (col == endColumn) {
-        cell.border = {
-            left: { style: borderStyle },
-            bottom: { style: 'medium' },
-            right: { style: 'medium' }
-            };
-      } else {
-      cell.border = {
-        right: { style: borderStyle },
-        left: { style: borderStyle },
-        bottom: { style: 'medium' }
-      };
-    }
-  }
-}
-
-  function applyBorderToCells(worksheet, startRow, endRow, startColumn, endColumn, borderStyle) {
-    for (let row = startRow; row <= endRow; row++) {
-        for (let col = startColumn; col <= endColumn; col++) {
-            const cell = worksheet.getCell(`${String.fromCharCode(64 + col)}${row}`);
-            if (col == endColumn) {
-                cell.border = {
-                    top: { style: borderStyle },
-                    bottom: { style: borderStyle },
-                    right: { style: 'medium' },
-                    left: { style: borderStyle }
-                };} else {
-                cell.border = {
-                    top: { style: borderStyle },
-                    bottom: { style: borderStyle },
-                    right: { style: borderStyle },
-                    left: { style: borderStyle }
-                };}
-            }
-        }
-    }
-
-  function applyRowColorToCells(worksheet, rowIndex, startColumn, endColumn) {
-    for (let col = startColumn; col <= endColumn; col++) {
-      const cell = worksheet.getCell(`${String.fromCharCode(64 + col)}${rowIndex}`);
-      cell.fill = { type: 'pattern', pattern: 'solid', fgColor: { argb: 'DDDDDD' }
-      };
-    }
-  }
-
-applyRowBorderToCells(worksheet, 12, 1, 9, 'medium');
-applyRowBorderToCells(worksheet, 13, 1, 9, 'medium');
-applyOneRowBorderToCells(worksheet, 13, 1, 9, 'thin');
-applyRowBorderToCells(worksheet, 15, 1, 14, 'medium');
-applyRowBorderToCells(worksheet, 16, 1, 14, 'medium');
-applyBorderToCells(worksheet, 17, 24, 1, 14, 'thin');
-applyRowColorToCells(worksheet, 12, 1, 9);
-applyRowColorToCells(worksheet, 15, 1, 14);
-applyRowColorToCells(worksheet, 16, 1, 14);
-applyRowBorderToCells(worksheet, 25, 1, 14, 'medium');
-
-//   worksheet.getRow(12).font = boldFont;
-//   worksheet.getRow(15).font = boldFont;
-//   worksheet.getRow(16).font = boldFont;
-
-worksheet.getCell('A12').value = 'Katedra';
-worksheet.getCell('C12').value = 'Studij';
-worksheet.getCell('D12').value = 'ak. god.';
-worksheet.getCell('E12').value = 'stud. god.';
-worksheet.getCell('F12').value = 'početak turnusa';
-worksheet.getCell('G12').value = 'kraj turnusa';
-worksheet.getCell('H12').value = 'br sati predviđen programom';
-worksheet.getCell('A15').value = 'Redni broj';
-worksheet.getCell('B15').value = 'Nastavnik/Suradnik';
-worksheet.getCell('C15').value = 'Zvanje';
-worksheet.getCell('D15').value = 'Status';
-worksheet.getCell('E15').value = 'Sati nastave';
-worksheet.getCell('E16').value = 'pred';
-worksheet.getCell('F16').value = 'sem';
-worksheet.getCell('G16').value = 'vjež';
-worksheet.getCell('H15').value = 'Bruto satnica predavanja (EUR)';
-worksheet.getCell('I15').value = 'Bruto satnica seminari (EUR)';
-worksheet.getCell('J15').value = 'Bruto satnica vježbe (EUR)';
-worksheet.getCell('K15').value = 'Bruto iznos';
-worksheet.getCell('K16').value = 'pred';
-worksheet.getCell('L16').value = 'sem';
-worksheet.getCell('M16').value = 'vjež';
-worksheet.getCell('N15').value = 'Ukupno za isplatu (EUR)';
-worksheet.getCell('A25').value = 'UKUPNO';
+    //   const jsonData = fs.readFileSync('data.json', 'utf8');
+    //   const data = JSON.parse(jsonData);
 
 
-  // Save the workbook
-  await workbook.xlsx.writeFile('projectTwo.xlsx');
-  console.log('Excel file generated!');
+    //   data.forEach((row, index) => {
+    //     const rowIndex = index + 12; // Add 12 to account for the preceding rows
+    //     worksheet.addRow(row).commit();
+    //   });
+
+    await workbook.xlsx.writeFile('projectTwo.xlsx');
+      console.log('Excel file generated!');
 }
 
 generateExcelTable().catch((error) => {
